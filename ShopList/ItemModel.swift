902513f51f697c762d5889303edf0542cbc6d0c4
zyx-0314 <ramirezian037@gmail.com>
2024-08-19 15:37:24 +0800

    feat: added funda code but this is not complete

---
+++ ShopList/ItemModel.swift
@@ -0,0 +1,12 @@
+//
+//  ItemModel.swift
+//  ShopList
+//
+//  Created by Mac User on 8/19/24.
+//
+
+import UIKit
+
+class ItemModel: NSObject {
+
+}
---
+++ ShopList/ProductSpecificViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ProductSpecificViewController.swift
+//  ShopList
+//
+//  Created by Mac User on 8/19/24.
+//
+
+import UIKit
+
+class ProductSpecificViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
